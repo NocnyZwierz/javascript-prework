de89d8e951e7199b04677ec57a49c6d2906f4d1c
Paweł <p.grabczak@cognitionhub.com>
2024-08-01 20:49:11 +0200

    komputer

--- js/script.js
+++ js/script.js
@@ -1,21 +1,17 @@
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-let playerInput = prompt ('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-//NTUI - Never trust user input
-if (playerInput <1 || playerInput >3) {
-    prompt (" wybierz jeszcze raz")
-} 
 
-console.log('Gracz wpisał: ' + playerInput);
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-let playerMove = 'nieznany ruch';
+let computerMove = 'nieznany ruch';
 
-if (playerInput == '1') {
-    playerMove = 'kamień';
-} else if (playerInput == '2') {
-    playerMove = 'papier';
-} else if (playerInput == '3') {
-    playerMove = 'nożyce';
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if (randomNumber == 2) {
+    computerMove = 'papier';
+} else if (randomNumber == 3) {
+    computerMove = 'nożyce'
 }
 
-  printMessage('Twój ruch to: ' + playerMove);
+printMessage('Mój ruch to: ' + computerMove);
 

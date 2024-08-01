b1efd018b11ad2324745a0752adc754e38b4e78a
Paweł <p.grabczak@cognitionhub.com>
2024-08-01 20:15:26 +0200

    zadanie 4.4

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-let computerMove = `kamień`;
-let playerMove = 'papier';
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-//test
+let playerInput = prompt ('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+//NTUI - Never trust user input
+if (playerInput <1 || playerInput >3) {
+    prompt (" wybierz jeszcze raz")
+} 
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if (playerInput == '1') {
+    playerMove = 'kamień';
+} else if (playerInput == '2') {
+    playerMove = 'papier';
+} else if (playerInput == '3') {
+    playerMove = 'nożyce';
+}
+
+  printMessage('Twój ruch to: ' + playerMove);
+

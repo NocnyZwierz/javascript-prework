9427a1c453e67c423f3d3d45e91c18047eeb9021
Paweł <p.grabczak@cognitionhub.com>
2024-08-01 20:59:32 +0200

    wynik gry

--- js/script.js
+++ js/script.js
@@ -31,3 +31,20 @@ if(playerInput == '1') {
 
 printMessage('Twój ruch to: ' + playerMove);
 
+if (computerMove == 'kamień' && playerMove == 'papier') {
+    printMessage('Ty wygrywasz!');
+  } else if (computerMove == 'kamień' && playerMove == 'nożyce') {
+    printMessage('Komputer wygrywa!');
+  } else if (computerMove == 'papier' && playerMove == 'kamień') {
+    printMessage('Komputer wygrywa!');
+  } else if (computerMove == 'papier' && playerMove == 'nożyce') {
+    printMessage('Ty wygrywasz!');
+  } else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+    printMessage('Ty wygrywasz!');
+  } else if (computerMove == 'nożyce' && playerMove == 'papier') {
+    printMessage('Komputer wygrywa!');
+  } else if (computerMove === playerMove) {
+    printMessage('Remis!');
+  } else {
+    printMessage('Nieznany ruch!');
+  }
